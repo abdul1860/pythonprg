e5641ed0a033502e31562fa28e023e969a6800f7
Abdul Belgaumi <abdul.rehman1860@gmail.com>
2023-11-29 22:43:35 +0530

    Improvement

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 n=int(input("Enter a Number"))
+print("Numbers from 1 to ",n," are")
 for i in range(n):
     if i%2==0:  
-        print(i)    #prints even numbers
+        print(f"{i} is Even")    #prints even numbers
     else:
-        print(i)
+        print(f"{i} is Odd")
